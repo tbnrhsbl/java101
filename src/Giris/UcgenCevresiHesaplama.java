b321ce1643dcf29edfbf2ffd63ebfd27fd0372ae
Tubanur Hasbal <tubanur_tuba@outlook.com>
2021-05-11 06:15:23 +0300

    -

---
+++ src/Giris/UcgenCevresiHesaplama.java
@@ -0,0 +1,32 @@
+package Giris;
+import java.util.Scanner;
+
+public class UcgenCevresiHesaplama {
+    public static void main(String[]args){
+
+        double a,b,c, uzunluk,alan;
+
+
+        Scanner giris = new Scanner(System.in);
+        System.out.print("1. Kenarı Giriniz : ");
+        a= giris.nextInt();
+        System.out.print("2. Kenarı Giriniz : ");
+        b=giris.nextInt();
+        System.out.print("3. Kenarı giriniz : ");
+        c=giris.nextInt();
+
+
+        uzunluk = (a+b+c)/2;
+        alan = Math.sqrt( (uzunluk - a)*(uzunluk - b)*(uzunluk - c));
+        System.out.println("Üçgenin Alanı : "+ alan);
+
+
+
+
+
+
+
+
+
+    }
+}
