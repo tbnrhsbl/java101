af3ed02b8ada5500d551cecbfa073bd108a2363c
Tubanur Hasbal <tubanur_tuba@outlook.com>
2021-05-11 06:17:49 +0300

    -

---
+++ src/Giris/HipotenusHesaplama.java
@@ -0,0 +1,26 @@
+package Giris;
+import java.util.Scanner;
+
+public class HipotenusHesaplama {
+    public static void main(String[]args){
+
+        //Değişkenlerimizi oluşturalım.
+
+        int a,b;
+        double c;
+
+        Scanner veri =new Scanner(System.in);
+        System.out.print("1. Kenarı Giriniz : ");
+        a=veri.nextInt();
+        System.out.print("2. Kenarı Giriniz : ");
+        b=veri.nextInt();
+
+        c= Math.sqrt((a*a)+(b*b));
+        System.out.println("Hipotenüs : " + c);
+
+
+
+
+
+    }
+}
