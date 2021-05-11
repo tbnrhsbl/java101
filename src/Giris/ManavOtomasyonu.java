0ae4bbb5a226cea0976daae72815b1cacede7749
Tubanur Hasbal <tubanur_tuba@outlook.com>
2021-05-12 00:07:34 +0300

    -

---
+++ src/Giris/ManavOtomasyonu.java
@@ -0,0 +1,29 @@
+package Giris;
+import java.util.Scanner;
+
+public class ManavOtomasyonu {
+    public static void main(String[]args){
+
+        double armut=2.14, elma=3.67, domates=1.11,muz=0.95, patlican=5.00 ,total ;
+        Scanner fiyat = new Scanner(System.in);
+
+        System.out.print("Kaç kilo armut aldıgınızı yazınız lütfen :");
+        armut=fiyat.nextDouble();
+        System.out.print("Kaç kilo elma aldıgınızı yazınız lütfen :");
+        elma=fiyat.nextDouble();
+        System.out.print("Kaç kilo domates aldıgınızı yazınız lütfen :");
+        domates=fiyat.nextDouble();
+        System.out.print("Kaç kilo muz aldıgınızı yazınız lütfen :");
+        muz=fiyat.nextDouble();
+        System.out.print("Kaç kilo patlıcan aldıgınızı yazınız lütfen :");
+        patlican=fiyat.nextDouble();
+
+        total=armut+elma+muz+domates+patlican;
+        System.out.println("Toplam tutar : "+ total + "TL");
+
+
+
+        }
+
+    }
+
---
+++ src/Giris/VucutKitleİndeksi.java
@@ -0,0 +1,19 @@
+package Giris;
+import java.util.Scanner;
+public class VucutKitleİndeksi {
+    public static void main(String[]args){
+
+        double boy , kilo , sonuc ;
+
+        Scanner bilgi = new Scanner(System.in);
+
+        System.out.print("Lütfen Kilonuzu Giriniz : ");
+        kilo=bilgi.nextDouble();
+        System.out.print("Lütfen Boyunuzu 'Metre Cinsinde' Giriniz :");
+        boy=bilgi.nextDouble();
+
+        sonuc = kilo / (boy * boy);
+
+        System.out.println("Vücut Kitle İndeksiniz : " + sonuc);
+    }
+}
