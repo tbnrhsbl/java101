3186d115c35b4755cc1ccf0715ba3acf8c40d352
Tubanur Hasbal <tubanur_tuba@outlook.com>
2021-06-01 11:21:18 +0300

    -

--- src/Giris/CinZoydagiHesaplayanProgram.java
+++ src/Giris/CinZoydagiHesaplayanProgram.java
@@ -6,48 +6,44 @@ public class CinZoydagiHesaplayanProgram {
     public static void main(String[] args) {
 
         int dogumYili;
-        String burc;
+        String burc="";
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Lütfen Doğum Tarihinizi Yıl Olarak Giriniz : ");
+        System.out.print("Lütfen Doğum Tarihinizi Yıl Olarak Giriniz : ");
         dogumYili = input.nextInt();
 
         if (dogumYili % 12 == 0) {
             burc = "Maymun";
-            System.out.println("Çin Zoydağı Burcunuz :" + burc);
         } else if (dogumYili % 12 == 1) {
             burc = "Horoz";
-            System.out.println("Çin Zoydağı Burcunuz :" + burc);
         } else if (dogumYili % 12 == 2) {
             burc = "Köpek";
-            System.out.println("Çin Zoydağı Burcunuz :" + burc);
         } else if (dogumYili % 12 == 3) {
             burc = "Domuz";
-            System.out.println("Çin Zoydağı Burcunuz :" + burc);
         } else if (dogumYili % 12 == 4) {
             burc = "Fare";
-            System.out.println("Çin Zoydağı Burcunuz :" + burc);
         } else if (dogumYili % 12 == 5) {
             burc = "Öküz";
-            System.out.println("Çin Zoydağı Burcunuz :" + burc);
         } else if (dogumYili % 12 == 6) {
             burc = "Kaplan";
-            System.out.println("Çin Zoydağı Burcunuz :" + burc);
         } else if (dogumYili % 12 == 7) {
             burc = "Tavşan";
-            System.out.println("Çin Zoydağı Burcunuz :" + burc);
         } else if (dogumYili % 12 == 8) {
             burc = "Ejderha";
-            System.out.println("Çin Zoydağı Burcunuz :" + burc);
         } else if (dogumYili % 12 == 9) {
             burc = "Yılan";
-            System.out.println("Çin Zoydağı Burcunuz :" + burc);
         } else if (dogumYili % 12 == 10) {
             burc = "At";
-            System.out.println("Çin Zoydağı Burcunuz :" + burc);
         } else if (dogumYili % 12 == 11) {
             burc = "Koyun";
+        }
+        if (dogumYili > 12 && dogumYili < 0){
+            System.out.println("Hatalı giriş yaptınız.");
+        } else {
             System.out.println("Çin Zoydağı Burcunuz :" + burc);
         }
+
+
+
     }
 }
