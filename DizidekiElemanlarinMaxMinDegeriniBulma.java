7a98c05c971d1af05b0c3eabdcdbfbe7c99b51ae
tubanur hasbal <77538172+tbnrhsbl@users.noreply.github.com>
2022-10-30 22:46:27 +0300

    Add files via upload

---
+++ DizidekiElemanlarinMaxMinDegeriniBulma.java
@@ -0,0 +1,22 @@
+public class DizidekiElemanlarinMaxMinDegeriniBulma {
+    public static void main(String[] args) {
+        int[] list = {56, 34, 1, 8, 101, -2, -33};
+        //min = list[0] , min <lit [1] ? min=list[1]: ''
+        //max=list[0]
+
+        int min = list[0];
+        int max = list[0];
+
+        for (int i : list) {
+            if (i < min) {
+                min = i;
+            }
+            if (i > max) {
+                max = i;
+            }
+        }
+        System.out.println("Minimum Değer : "+min);
+        System.out.println("Maximum Değer : "+max);
+
+    }
+}
